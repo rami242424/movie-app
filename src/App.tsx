1510df4e08c9ed51b9225bb1a6f8044f83524b58
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-13 20:15:18 +0900

    searchbar setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+
 function App(){
+  const [movieKeyword, setMovieKeyword] = useState("");
   return (
     <>
+      <SearchBar 
+        movieKeyword={movieKeyword}
+        setMovieKeyword={setMovieKeyword}
+      />
     </>
   );
 }
 
 export default App;
+
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+
+export interface Props {
+    movieKeyword: string;
+    setMovieKeyword: (value: string) => void;
+}
+
+function SearchBar({movieKeyword, setMovieKeyword}:Props){
+    const handleKeywordChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setMovieKeyword(e.target.value);
+    }
+    const handleSearch = () => {
+
+    }
+    return(
+        <>
+            <input value={movieKeyword} onChange={handleKeywordChange}/>
+            <button onClick={handleSearch}>Search</button>
+        </>
+    );
+}
+
+export default SearchBar;
