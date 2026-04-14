41e3b78e7af7521bf0c9c8a31027b46bfb7725cb
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-14 11:41:03 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import SearchBar from "./components/SearchBar";
 
 function App(){
   const [movieKeyword, setMovieKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string|null>(null);
+  const [movie, setMovie] = useState<string[]>([]);
   return (
     <>
       <SearchBar 
         movieKeyword={movieKeyword}
         setMovieKeyword={setMovieKeyword}
+        loading = {loading}
+        setLoading = {setLoading}
+        error = {error}
+        setError = {setError}
+        movie = {movie}
+        setMovie = {setMovie}
       />
     </>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,19 +3,46 @@ import type React from "react";
 export interface Props {
     movieKeyword: string;
     setMovieKeyword: (value: string) => void;
+    loading: boolean;
+    setLoading: (value:boolean) => void;
+    error: string | null;
+    setError: (value: string|null) => void;
+    movie: string[];
+    setMovie: (value: string[]) => void;
 }
 
-function SearchBar({movieKeyword, setMovieKeyword}:Props){
+ 
+const API_KEY = "";
+
+function SearchBar({movieKeyword, setMovieKeyword,loading,setLoading,error,setError,movie,setMovie}:Props){
+    
     const handleKeywordChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setMovieKeyword(e.target.value);
     }
-    const handleSearch = () => {
-        
+    const handleSearch = async() => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(``);
+            if(!response.ok) throw new Error("검색어를 찾을 수 없습니다.");
+        } catch(error){
+            if( error instanceof Error){    
+                console.log(error.message);
+                setError(error.message);
+            }
+        } finally{
+            setLoading(false);
+        }
+
+
     }
     return(
         <>
             <input value={movieKeyword} onChange={handleKeywordChange}/>
             <button onClick={handleSearch}>Search</button>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {movie && <p>{movie}</p>}
         </>
     );
 }
