bda6584a429aebd63cf5be368f317827cbb94915
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-15 11:53:14 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,10 @@
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import type { Movie } from "./types/movie";
-import MovieList from "./components/MovieList";
-
-
-const API_KEY = "ae39336185873212a3317f6c4e235bbf";
-type Status = "idle" | "loading" | "success" | "error";
-
 function App(){
-  const [movieKeyword, setMovieKeyword] = useState("");
-  const [status, setStatus] = useState<Status>("idle");
-  const [error, setError] = useState<string|null>(null);
-  const [movie, setMovie] = useState<Movie[]>([]);
-  const handleSearch = async() => {
-      if(!movieKeyword.trim()) return;
-
-        setStatus("loading");
-        setError(null);
-        try {
-            const response = await fetch(
-              `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(movieKeyword)}&api_key=${API_KEY}`);
-            if(!response.ok) throw new Error("API 요청 실패");
-            const json = await response.json();
-              setMovie(json.results);
-              setStatus("success");
-        } catch(error){
-          if( error instanceof Error){    
-              //console.log(error.message);
-              setError(error.message);
-          }
-        } finally{
-          //
-        }
-    }
-  return (
+  return(
     <>
-      <SearchBar 
-        movieKeyword={movieKeyword}
-        setMovieKeyword={setMovieKeyword}
-        handleSearch = {handleSearch}
-      />
-      <MovieList movie={movie} status={status} error={error} />
+    <input/>
+    <button>Search</button>
     </>
   );
 }
 
 export default App;
-
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,35 +1,7 @@
-import type React from "react";
-
-export interface Props {
-  movieKeyword: string;
-  setMovieKeyword: (value: string) => void;
-  handleSearch: () => void;
-}
-
-function SearchBar({ movieKeyword, setMovieKeyword, handleSearch }: Props) {
-  const handleKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMovieKeyword(e.target.value);
-  };
-
-  return (
-    <div className="flex justify-center gap-2 p-4">
-      <input
-        value={movieKeyword}
-        onChange={handleKeywordChange}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") handleSearch();
-        }}
-        placeholder="영화를 검색하세요..."
-        className="border border-gray-300 rounded-lg px-4 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
-
-      <button
-        onClick={handleSearch}
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-      >
-        Search
-      </button>
-    </div>
+function SearchBar(){
+  return(
+    <>
+    </>
   );
 }
 
