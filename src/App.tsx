97359d2a1116e3f8049646e1a74e9e4ff68bce06
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-15 21:56:41 +0900

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -55,28 +55,6 @@ function App(){
         searching={searching}
         movie={movie}
       />
-      {/* {searching && (
-        movie.length === 0 
-        ? (<div>검색결과가 없습니다.</div>)
-        : (
-          <ul>
-            {movie.map((m) => 
-              <li key={m.id}>
-                <h3>{m.title} ({m.release_date})</h3>
-                {m.poster_path && (
-                    <img src={`https://image.tmdb.org/t/p/w200/${m.poster_path}`}/>
-                  )
-                }
-                <p>Summary : {m.overview.length > 180 
-                  ? m.overview.slice(0, 180) + "..."
-                  : m.overview
-                  }
-                </p>
-                <p>🌟{m.vote_average}</p>
-              </li>
-            )}
-          </ul>
-      ))} */}
     </>
   );
 }
