6cfbd43c3779befdf625667978e8a7b91008a228
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-15 14:47:17 +0900

    app return: ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import { useState } from "react";
 
 const API_KEY = "ae39336185873212a3317f6c4e235bbf";
 
+interface MovieProps {
+  title: string;
+  release_date: string;
+  vote_average: number;
+  overview: string;
+  poster_path: string;
+  id: number;
+}
+
 function App(){
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string|null>(null);
   const [movieKeyword, setMovieKeyword] = useState("");
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState<MovieProps[]>([]);
 
   const handleKeywordChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setMovieKeyword(e.target.value);
@@ -23,20 +32,31 @@ function App(){
       const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(movieKeyword)}&api_key=${API_KEY}`);
       if(!response.ok) throw new Error("API 요청 실패");
       const json = await response.json();
-      //console.log(json.results[0].title, "json좀봐봐");
       setMovie(json.results);
     } catch(error){
       if( error instanceof Error){
         setError(error.message);
       }
-    } finally {
-      setLoading(false);
-    }
+    } 
+    setLoading(false);
   }
+
   return(
     <>
       <input value={movieKeyword} placeholder="영화 제목을 검색하세요." onChange={handleKeywordChange}/>
       <button onClick={handleSearch}>Search</button>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {movie && (
+        <ul>
+          {movie.map((m) => 
+            <li key={m.id}>
+              <h3>{m.title} ({m.release_date})</h3>
+              
+            </li>
+          )}
+        </ul>
+      )}
     </>
   );
 }
