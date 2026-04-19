633d4740ca73af22adb0bcd7e11de496a4af63fd
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-19 20:21:51 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,20 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 export interface IMovies {
   id: number;
   overview: string;
-  poster_path: string;
+  poster_path: string |null;
   release_date: string;
   title: string;
   vote_average: number;
 }
+
+interface MoviesResponse {
+  page: number;
+  results: IMovies[];
+  total_pages: number;
+  total_results: number;
+}
+
+
 function App(){
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string|null>(null);
@@ -30,9 +39,12 @@ function App(){
       const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(keyword)}&api_key=${API_KEY}`);
       if(!response.ok) throw new Error ("API 연결 실패");
 
-      const json = await response.json();
+      const json :MoviesResponse = await response.json();
+      if (!json || !Array.isArray(json.results)) {
+          throw new Error("잘못된 응답 구조");
+        }
       setMovies(json.results);
-      console.log(json.results)
+
     } catch(error){
       if(error instanceof Error){
         setError(error.message);
@@ -44,17 +56,32 @@ function App(){
 
   return(
     <>
-      <input value={keyword} placeholder="영화제목을 검색하세요." onChange={handleChange}/>
+      <input 
+        value={keyword} 
+        placeholder="영화제목을 검색하세요." 
+        onChange={handleChange}
+        onKeyDown={(e) => {
+          if(e.key === "Enter")  handleSearch();
+        }}
+      
+      />
       <button onClick={handleSearch}>Search</button>
       {loading && <div>Loading...</div>}
       {error && <div>{error}</div>}
-      {movies && (
+      {movies.length > 0 && (
         <ul>
           {movies.map((movie) => (
-            <li>
+            <li key={movie.id}>
               <h3>{movie.title}</h3>
               <p>{movie.release_date}</p>
-              <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>
+              {movie.poster_path ? (
+                <img 
+                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                ) : (
+                <div>No image</div>
+                )}
               <p>{movie.overview}</p>
               <p>🌟{movie.vote_average}</p>
             </li>
