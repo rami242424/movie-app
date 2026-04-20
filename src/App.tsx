0fccdd7cc4efee369f9512a1678df361874b0793
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-20 10:02:13 +0900

    onKeyDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,62 @@
 import { useState } from "react";
 import type { IMovies } from "./types/movie";
 
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App(){
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string|null>(null);
   const [keyword, setKeyword] = useState("");
   const [movies, setMovies] = useState<IMovies[]>([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  }
+  const searchMovie = async() => {
+    if(!keyword.trim()) return;
+    setLoading(true);
+    setError(null);
+    setMovies([]);
+
+    try{
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(keyword)}&api_key=${API_KEY}`);
+      if(!response.ok) throw new Error("API 연결 실패");
+      const json = await response.json();
+      setMovies(json.results);
+    } catch(error){
+      if(error instanceof Error){
+        setError(error.message);
+      }
+    } finally {
+      setLoading(false);
+      setHasSearched(true);
+    }
+  }
   return(
     <>
-      <input/>
-      <button>Search</button>
+      <input 
+        value={keyword}
+        placeholder="영화이름을 검색하세요."
+        onChange={handleInputChange}
+        onKeyDown={(e) => {
+          if(e.key === "Enter") searchMovie();
+        }}
+      />
+      <button
+        onClick={searchMovie}
+        // disabled
+      >
+        Search
+      </button>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {!loading && !error && movies.length === 0 && hasSearched && <div>검색결과가 없습니다.</div>}
+      {movies.length > 0 && (
+        movies.map((movie) => (
+          <li>{movie.title}</li>
+        ))
+      )}
     </>
   );
 }
