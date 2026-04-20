3d5859888c8c2efcba5a3bb693e3f21605e4a46b
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-20 09:24:18 +0900

    reset_settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import type { IMovies } from "./types/movie";
+
+
 function App(){
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string|null>(null);
+  const [keyword, setKeyword] = useState("");
+  const [movies, setMovies] = useState<IMovies[]>([]);
   return(
     <>
       <input/>
