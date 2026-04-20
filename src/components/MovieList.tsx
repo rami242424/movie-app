bb17f17c8cf7a8e45193b7ee945013fea602cccf
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-20 19:50:54 +0900

    MovieList에 MovieSkeleton  추가

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,21 +1,31 @@
 import type { IMovies } from "../types/movie";
 import MovieItem from "./MovieItem";
+import MovieSkeleton from "./MovieSkeleton";
 
 interface IMovieListProps {
   movies: IMovies[];
+  isLoading?: boolean;
 }
 
-function MovieList({movies}:IMovieListProps){
+function MovieList({movies, isLoading}:IMovieListProps){
   return(
     <ul
       className="grid grid-cols-3 gap-4"
     >
-      {movies.map((movie) => (
+      {isLoading
+        ? Array.from({ length: 9}).map((_,i) => (
+          <MovieSkeleton key={i}/>
+        ))
+        : movies.map((movie) => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))
+      }
+      {/* {movies.map((movie) => (
         <MovieItem
           key={movie.id}
           movie={movie}
         />
-      ))}
+      ))} */}
     </ul>
   );
 }
