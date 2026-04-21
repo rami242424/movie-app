2c93a8929afb0f036cea56d91e47716651a2b012
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-21 19:12:00 +0900

    app tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,27 @@ function App(){
   const [keyword, setKeyword] = useState("");
   const { fetchState, SearchBtn } = useMovies(keyword);
   return(
-    <>
-      <SearchBar 
-        setKeyword={setKeyword}
-        SearchBtn={SearchBtn}
-        keyword={keyword}
-      />
-      {fetchState.status === "loading" && (
-        <MovieList movies={[]} isLoading={true}/>
-      )}
-      {fetchState.status === "error" && <div>{fetchState.error}</div>}
-      {fetchState.status === "success" && (
-        fetchState.data.length > 0 
-        ? (
-          <MovieList movies={fetchState.data}/>
-        ) : (
-          <div>검색된 결과가 없습니다.</div>
-        )
-      )}
-    </>
+    <div className="min-h-screen bg-gray-200 flex felx-col items-center py-10">
+      <div className="w-full max-w-3xl">
+        <SearchBar 
+          setKeyword={setKeyword}
+          SearchBtn={SearchBtn}
+          keyword={keyword}
+        />
+        {fetchState.status === "loading" && (
+          <MovieList movies={[]} isLoading={true}/>
+        )}
+        {fetchState.status === "error" && <div>{fetchState.error}</div>}
+        {fetchState.status === "success" && (
+          fetchState.data.length > 0 
+          ? (
+            <MovieList movies={fetchState.data}/>
+          ) : (
+            <div>검색된 결과가 없습니다.</div>
+          )
+        )}
+      </div>
+    </div>
   );
 }
 
