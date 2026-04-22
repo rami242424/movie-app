5554a6ec1ee10be84eabab14c4e93ef8090f755f
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-22 12:24:33 +0900

    MovieItem nf-vers

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -8,7 +8,7 @@ function MovieItem({movie}:IMovieItemProps){
   return(
     <>
       <li
-        className="bg-white rounded shadow p-3 flex flex-col gap-2"
+        className="bg-[#1f1f1f] rounded shadow p-3 flex flex-col gap-2"
       >
         {movie.poster_path ?
           (<img
@@ -17,16 +17,16 @@ function MovieItem({movie}:IMovieItemProps){
             src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
           />
         ) : (
-          <div className="w-full aspect-2/3 bg-gray-200 flex items-center justify-center text-gray-400 rounded"
+          <div className="w-full aspect-2/3 bg-[#4a4848] flex items-center justify-center text-gray-400 rounded"
           >
             🎬 No Image
           </div>
         )
         }
-        <h2 className="font-semibold text-sm leading-tight">{movie.title}</h2>
-        <p className="font-mono text-xs text-gray-500">{movie.release_date}</p>
-        <p className="line-clamp-3 text-sm">{movie.overview}</p>
-        <p className="text-sm">🌟{movie.vote_average}</p>
+        <h2 className="font-semibold text-sm leading-tight text-white">{movie.title}</h2>
+        <p className="font-mono text-xs text-gray-400">{movie.release_date}</p>
+        <p className="line-clamp-3 text-sm text-white">{movie.overview}</p>
+        <p className="text-sm text-white">🌟{movie.vote_average}</p>
       </li>
     </>
   );
