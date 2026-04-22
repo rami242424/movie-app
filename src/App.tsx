85aacc31582cbcbd166f4e0eb59bb23a53a1b9e3
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-22 12:07:16 +0900

    app np-vers

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(){
   const [keyword, setKeyword] = useState("");
   const { fetchState, SearchBtn } = useMovies(keyword);
   return(
-    <div className="min-h-screen bg-gray-200 flex flex-col items-center py-10">
+    <div className="min-h-screen bg-[#141414] flex flex-col items-center py-10">
       <div className="w-full max-w-3xl">
         <SearchBar 
           setKeyword={setKeyword}
