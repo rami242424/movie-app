127002434d9102f590cc312a85212339dc8d28b6
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-24 10:00:00 +0900

    again

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,16 +1,7 @@
-import type { IMoviesProps } from "../types/movie";
-
-interface IMovieItemProp {
-  movie: IMoviesProps;
-}
-function MovieItem ({movie}:IMovieItemProp){
-  return( 
-    <>
-      <li key={movie.id}>
-        <h3>{movie.title}</h3>
-        <p>{movie.overview}</p>
-      </li>
-    </>
+function MovieItem(){
+  return(
+    <></>
   );
 }
-export default MovieItem
+
+export default MovieItem;
