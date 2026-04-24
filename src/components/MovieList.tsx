1b0746e18ca690144c2da522fafa89324cee2228
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-24 10:00:00 +0900

    again

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,28 +1,6 @@
-import type { IMoviesProps } from "../types/movie";
-import MovieItem from "./MovieItem";
-import MovieSkeleton from "./MovieSkeleton";
-
-interface IMovieListProps{
-  movies: IMoviesProps[]; 
-  isLoading?: boolean; 
-} 
-
-function MovieList({movies, isLoading}:IMovieListProps){
+function MovieList(){
   return(
-    <>
-      <ul>
-        {isLoading 
-        ? (
-          Array.from({length: 9}).map((_,i) =>
-          <MovieSkeleton key={i}/>)
-        ) : (
-          movies.map((movie) => (
-            <MovieItem movie={movie} />
-            ))
-        )}
-      </ul>
-    </>
-  )
+    <></>
+  );
 }
-
 export default MovieList;
