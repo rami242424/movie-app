f6123810d4988df9853a85733d2300b5407d9cc1
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-25 10:00:00 +0900

    again

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,6 @@
-
-interface ISearchBar {
-  setKeyword: (value:string) => void;
-  keyword: string;
-  handleSearchBtn: () => void;
-} 
-
-function SearchBar({setKeyword, keyword, handleSearchBtn}:ISearchBar){
-  const searchInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.target.value);
-  }
+function SearchBar(){
   return(
-    <>
-      <input 
-        onKeyDown={(e) => e.key === "Enter" && handleSearchBtn()} 
-        value={keyword} 
-        placeholder="영화 제목을 검색해주세요." 
-        onChange={searchInputChange}/>
-      <button onClick={handleSearchBtn}>Search</button>
-    </>
+    <></>
   );
 }
 
