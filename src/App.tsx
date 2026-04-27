d5da512c79fca22dddb8fbebcc821c96e09a91ad
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-27 12:12:36 +0900

    복기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,77 @@
+import { useState } from "react";
+import type { IMoviesProps } from "./types/movie";
+
+const API_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
 function App(){
+  const [keyword, setKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string|null>(null);
+  const [movies, setMovies] = useState<IMoviesProps[]>([]);
+  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  }
+  const searchBtn = async() => {
+    if(!keyword.trim()) return;
+
+    setLoading(true);
+    setError(null);
+    setMovies([]);
+
+    try{
+
+      const response = await fetch (
+        `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(keyword)}`,
+          {
+            headers: {
+              Authorization: `Bearer ${API_TOKEN}`
+            }
+          }
+      );
+      if(!response.ok) throw new Error("API 연결 실패");
+      
+      const json = await response.json();
+      setMovies(json.results);
+    } catch(error){
+      if(error instanceof Error){
+        setError(error.message);
+      }
+    } finally{
+      setLoading(false);
+    }
+  }
   return(
-    <></>
+    <>
+      <input 
+        placeholder="영화이름을 검색하세요." 
+        value={keyword}
+        onChange={inputChange}
+        onKeyDown={(e) => {if((e.key) === "Enter") searchBtn()}}
+      />
+      <button
+        onClick={searchBtn}
+      >
+        Search
+      </button>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {!loading && !error && (
+        <ul>
+          {movies.map((movie) => (
+            <li>
+              <img
+                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <h2>{movie.title}</h2>
+              <p>{movie.release_date}</p>
+              <span>{movie.overview}</span>
+              <p>{movie.vote_average}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
