137f635797ea20aa8f4d01158506a2aa430b77fe
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-27 19:36:34 +0900

    복기:movieitem 분리

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,6 +1,22 @@
-function MovieItem(){
+import type { IMoviesProps } from "../types/movie";
+
+interface IMovieItem {
+  movie : IMoviesProps;
+}
+function MovieItem({movie}:IMovieItem){
   return(
-    <></>
+    <>
+      <li key={movie.id}>
+        <img
+          src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <h2>{movie.title}</h2>
+        <p>{movie.release_date}</p>
+        <span>{movie.overview}</span>
+        <p>{movie.vote_average}</p>
+      </li>
+    </>
   );
 }
 
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,4 +1,5 @@
 import type { IMoviesProps } from "../types/movie";
+import MovieItem from "./MovieItem";
 
 interface IMovieList {
   movies: IMoviesProps[];
@@ -8,16 +9,7 @@ function MovieList({movies}:IMovieList){
     <>
       <ul>
           {movies.map((movie) => ( 
-            <li key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <h2>{movie.title}</h2>
-              <p>{movie.release_date}</p>
-              <span>{movie.overview}</span>
-              <p>{movie.vote_average}</p>
-            </li>
+            <MovieItem movie={movie} key={movie.id}/>
           ))}
       </ul>
     </>
