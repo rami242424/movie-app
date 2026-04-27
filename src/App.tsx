e7af52770374e1c15520e298a37df325de960c03
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-27 19:33:27 +0900

    복기:movielist 분리_fetchState.map > movies로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useMovies } from "./hooks/useMovies";
 import SearchBar from "./components/SearchBar";
+import MovieList from "./components/MovieList";
 
 
 function App(){
@@ -19,20 +20,7 @@ function App(){
       {fetchState.status === "success" && (
         fetchState.data.length > 0 
         ? (
-          <ul>
-            {fetchState.data.map((movie) => ( 
-              <li key={movie.id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                  alt={movie.title}
-                />
-                <h2>{movie.title}</h2>
-                <p>{movie.release_date}</p>
-                <span>{movie.overview}</span>
-                <p>{movie.vote_average}</p>
-              </li>
-            ))}
-          </ul>
+          <MovieList movies={fetchState.data}/>
           ) : <div>검색결과가 없습니다.</div>
         )
       }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,6 +1,26 @@
-function MovieList(){
+import type { IMoviesProps } from "../types/movie";
+
+interface IMovieList {
+  movies: IMoviesProps[];
+}
+function MovieList({movies}:IMovieList){
   return(
-    <></>
+    <>
+      <ul>
+          {movies.map((movie) => ( 
+            <li key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <h2>{movie.title}</h2>
+              <p>{movie.release_date}</p>
+              <span>{movie.overview}</span>
+              <p>{movie.vote_average}</p>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 export default MovieList;
