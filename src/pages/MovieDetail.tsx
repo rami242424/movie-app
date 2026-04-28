46346f6227beb0ef71713bcebd78877b254daffd
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-28 12:28:39 +0900

    복기:MovieDetail

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -3,10 +3,10 @@ import { useMovieDetail } from "../hooks/useMovieDetail";
 
 function MovieDetail(){
     const { id } = useParams();
-    const {fetchState, setFetchState} = useMovieDetail("id");
+    const {fetchState} = useMovieDetail(id ?? "");
     return(
         <>
-            {fetchState.status === "success" && <div>{setFetchState}</div>}
+            {fetchState.status === "success" && <div>{fetchState.data.title}</div>}
         </>
     );
 }
