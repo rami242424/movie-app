b11ce60bb6aa764766c2d70b9b465bbc290f7c51
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-29 12:39:36 +0900

    useMovieDetail 복기

--- src/hooks/useMovieDetail.ts
+++ src/hooks/useMovieDetail.ts
@@ -1,3 +1,30 @@
-export function useMovieDetail(){
-    return {}
+import { useEffect, useState } from "react";
+import type { DetailFetchState } from "../types/movie";
+
+const API_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+
+export function useMovieDetail(id:string){
+    const [fetchState, setFetchState] = useState<DetailFetchState>({status:"idle"});
+    useEffect(() => {
+        const movieDetailFetch = async() => {
+            try{
+                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}`,
+                    {
+                        headers: {
+                        Authorization: `Bearer ${API_TOKEN}`
+                        }
+                    }
+                );
+                if(!response.ok) throw new Error("API 연결 실패");
+                const json = await response.json();
+                setFetchState({status:"success", data:json.results})
+            }catch(error){
+                if(error instanceof Error){
+                    setFetchState({status:"error", error:error.message});
+                }
+            }
+        }
+        movieDetailFetch()
+    }, [id]);
+    return {fetchState}
 }
--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useMovieDetail } from "../hooks/useMovieDetail";
+
 function MovieDetail(){
+    const { id } = useParams();    
+    const {fetchState} = useMovieDetail(id ?? "");
     return(
-        <></>
+        <>
+            {fetchState.status === "success" && (
+                <>
+                    <img src={`https://image.tmdb.org/t/p/w300/${fetchState.data.poster_path}`}/>
+                    <h3>{fetchState.data.title}</h3>
+                </>
+            )
+            }
+        </>
     );
 }
 
